8b2e2811c1eeb87ad80f0794d9cb86ea5d48ef2a
eryck46 <eryckeleuterio98@gmail.com>
2023-03-08 10:47:47 -0300

    aulaUm

---
+++ algoritmo.js
@@ -0,0 +1,15 @@
+let numeroUm = 0;
+const numeroDois = 10;
+let numeroTres = 20;           
+
+//diferença entre const  e var 
+//diferença entre let e var 
+  
+console.log(numeroUm)
+//para que serve o consile.log e como usá-lo.
+
+numeroUm = numeroDois;
+console.log(numeroUm)
+
+numeroUm = numeroTres;
+console.log (numeroUm)
